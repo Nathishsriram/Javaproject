2b0ce6a9e6d339f7733bb217b2e7367044e9f366
Nathishsriram <nathishsriram68@gmail.com>
2022-02-15 13:24:27 +0530

    my commit

---
+++ src/com/insituate/InsituateDetail.java
@@ -0,0 +1,15 @@
+package com.insituate;
+
+public class InsituateDetail {
+
+private void greensomr() {
+System.out.println("omr");
+}	
+private void greenstambaram() {
+System.out.println("tambaram");
+}	
+public static void main(String[] args) {
+	InsituateDetail info=new InsituateDetail();
+	info.greensomr();
+}
+}
